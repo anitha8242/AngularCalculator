cb493f711e6b27ec0b6793749a50882402697859
anitha8242 <105612259+anitha8242@users.noreply.github.com>
2024-02-05 13:42:41 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,28 @@
-def call(){
-  echo "hello-world"
+pipeline {
+  agent any
+    
+  tools {nodejs "node"}
+    
+  stages {
+        
+    stage('Git') {
+      steps {
+        git 'https://github.com/anitha8242/AngularCalculator.git'
+      }
+    }
+     
+    stage('Build') {
+      steps {
+        sh 'npm install'
+         sh 'npm install -g @angular/cli'
+      }
+    }  
+    
+            
+    stage('Test') {
+      steps {
+        sh 'ng build'
+      }
+    }
+  }
 }
